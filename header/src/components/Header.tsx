4353fb8c1612b9379ca929ae4afc4f96ff9e31e7
Rodrigo Pereira <rodrigo_pereira@outlook.com>
2024-04-30 15:19:11 -0300

    feat: add svg structure to header

---
+++ header/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import {ReactComponent as Logo} from '../assets/icons/logo.svg'
+
+export const Header = () => {
+  return (
+    <header>
+      <main>
+        <Logo />
+      </main>
+    </header>
+  )
+}
--- header/vite.config.ts
+++ header/vite.config.ts
@@ -1,10 +1,20 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import svgr from 'vite-plugin-svgr'
 import federation from '@originjs/vite-plugin-federation'
 
 export default defineConfig({
   plugins: [
     react(),
+    svgr({
+      svgrOptions: {
+        exportType: 'named',
+        ref: true,
+        svgo: false,
+        titleProp: true,
+      },
+      include: '**/*.svg',
+    }),
     federation({
       name: 'header-app',
       filename: 'remoteEntry.js',
