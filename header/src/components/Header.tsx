06376e73f83fa568af32e5329c2b8da37455e6f7
Rodrigo Pereira <rodrigo_pereira@outlook.com>
2024-04-30 16:39:32 -0300

    feat: style header responsive

--- header/src/components/Header.tsx
+++ header/src/components/Header.tsx
@@ -1,10 +1,14 @@
 import {ReactComponent as Logo} from '../assets/icons/logo.svg'
+import {ReactComponent as ShoppingBag} from '../assets/icons/shopping-bag.svg'
 
 export const Header = () => {
   return (
-    <header>
-      <main>
+    <header className="bg-primary flex flex-wrap justify-center p-4 drop-shadow-md">
+      <main className="flex justify-between items-center w-full max-w-screen-2xl">
         <Logo />
+        <button className="bg-secondary px-4 py-1 rounded-full">
+          <ShoppingBag />
+        </button>
       </main>
     </header>
   )
--- header/tailwind.config.js
+++ header/tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      primary: '#02D72F',
+      secondary: '#1B2126',
+    },
   },
   plugins: [],
 }
